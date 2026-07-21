bfdbcc953a2633d1add0a457ee3b7b81ba4c88ab
Jorge Onrubia Mojica <j.onrubia@ispges.com>
2026-07-21 20:44:09 +0200

    fix: disable auto-lint on save, restore Tab/leader+p buffer nav

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -7,12 +7,6 @@ return {
         javascript = { "eslint" },
         typescript = { "eslint" },
       }
-
-      vim.api.nvim_create_autocmd("BufWritePost", {
-        callback = function()
-          require("lint").try_lint()
-        end,
-      })
     end,
   },
 }
