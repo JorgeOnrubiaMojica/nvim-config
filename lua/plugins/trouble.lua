c6edbb528546ee16ed0883a79d7269e33db724f6
Jorge Onrubia Mojica <j.onrubia@ispges.com>
2026-07-21 17:37:49 +0200

    feat: add which-key, trouble, improve LSP setup
    - Add which-key.nvim for keymap discovery
    - Add trouble.nvim for structured diagnostics/references
    - Update LSP with blink.cmp capabilities (get_lsp_capabilities)
    - Add LspAttach autocommand with K (hover) and gD (declaration)
    - Enable format-on-save when LSP supports formatting

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {},
+    keys = {
+      { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>",        desc = "Diagnostics (Trouble)" },
+      { "<leader>xw", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
+      { "<leader>xs", "<cmd>Trouble symbols toggle<cr>",            desc = "Symbols (Trouble)" },
+      { "<leader>xl", "<cmd>Trouble loclist toggle<cr>",            desc = "Location List (Trouble)" },
+      { "<leader>xq", "<cmd>Trouble qflist toggle<cr>",             desc = "Quickfix List (Trouble)" },
+    },
+  },
+}
