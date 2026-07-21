9325903eb48efb51aa66b6792bc42b1cb44fba24
Jorge Onrubia <jorge.onrubia@visma.com>
2026-07-21 11:34:09 +0200

    fix(refactoring): lazy load solo cuando se use
    - Cargar refactoring.nvim solo al usar <leader>ef o <leader>iv
    - Evita race condition con plenary al inicio
    - Usa config() en vez de opts para setup explícito

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,17 +1,16 @@
 return {
-  -- Asegurar que plenary se carga primero
-  {
-    "nvim-lua/plenary.nvim",
-    lazy = false,
-    priority = 1000,
-  },
   {
     "ThePrimeagen/refactoring.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
     },
-    event = "VeryLazy", -- Cargar después de todo lo demás
-    opts = {},
+    keys = {
+      { "<leader>ef", mode = { "n", "x" } },
+      { "<leader>iv", mode = { "n", "x" } },
+    },
+    config = function()
+      require("refactoring").setup()
+    end,
   } 
 }
