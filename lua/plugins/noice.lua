b8e5177a04fff050c49a566a756b6fc09736c2aa
Jorge Onrubia Mojica <j.onrubia@ispges.com>
2026-07-21 20:46:11 +0200

    feat: configure noice messages, notify, and lsp progress

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -3,17 +3,33 @@ return {
     "folke/noice.nvim",
     event = "VeryLazy",
     opts = {
+      messages = {
+        view = "mini",
+        view_error = "mini",
+        view_warn = "mini",
+        view_history = "messages",
+      },
+      notify = {
+        view = "mini",
+      },
       lsp = {
         override = {
           ["vim.lsp.util.convert_input_to_markdown"] = true,
           ["vim.lsp.util.stylize_markdown"] = true,
           ["cmp.entry.get_documentation"] = true,
+          ["vim.lsp.util.show_line_diagnostics"] = false,
+        },
+        progress = {
+          enabled = true,
+          format = "lsp",
+          throttle = 1000 / 30,
         },
       },
       presets = {
         bottom_search = true,
         command_palette = true,
         long_message_to_split = true,
+        lsp_doc_border = true,
       },
     },
     dependencies = {
